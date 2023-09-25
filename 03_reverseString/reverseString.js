7ba9c0871cc8e8450b8029c9db73232b1e9ee4b7
pharma <92540444+GreatLoveV@users.noreply.github.com>
2023-09-25 17:48:33 +0100

    completed ex 3 tried adding characters from the end to the reversed string variable using the string.length - i)) the loop ends when i <= word.length

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    //find string length - 1 for final character
+    
+    let reversedString = ""
+    //keep increasing the substraction till the string ends at its first index
+    for (let i = 0; i <= word.length; i++){
+        reversedString += word.charAt(word.length - i);
+
+    }
+    return reversedString;
+    //push them all to a new variable and return it
 
 };
 
