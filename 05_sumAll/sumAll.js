c61658793de6a8afe5161605a86876f547f40477
pharma <92540444+GreatLoveV@users.noreply.github.com>
2023-10-04 14:22:53 +0100

    Completed ex5, solution was much different first condition to make sure the number is an integer or else return 'ERROR' declare a variable to hold the sum if and else if to determine which process to go through with, depending on the comparision between the two variables else return error if its negative return the sum and end the function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+    let sum = 0;
+    if (num2 > num1 && (num1 > 0 && num2 > 0)){
+        for(let i = 0; i < num2; i++){
+            sum += num1 + i;
+        }
+    }else if(num1 > num2 && (num1 > 0 && num2 > 0)){
+        for(let i = 0; i < num1; i++){
+            sum += num2 + i
+        }
+    }else{
+        return "ERROR";
+    }
+    return sum;
 };
 
 // Do not edit below this line
