235d01a3ebbdde556f6cb6d5b4bb1bd41be82db6
pharma <92540444+GreatLoveV@users.noreply.github.com>
2023-10-07 18:11:58 +0100

    completed exercise 6 with an unnecessarily long method forgot that you can just return the comparision expressions as booleans stored the conditional boolean statement in variable then return true or false after use if statements on the variable as per instructions on determining leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,55 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(year) {
+    let divisibleBy4;
+    if (year % 4 == 0){
+        divisibleBy4 = true
+    }else{
+        divisibleBy4 = false;
+    }
+    let divisibleBy100;
+    if (divisibleBy4 == true){
+        if (year % 100 == 0){
+            divisibleBy100 = true
+        }else {
+            divisibleBy100 = false
+        }
+    }
+    let divisibleBy400;
+    if (divisibleBy4 == true && divisibleBy100 == true){
+        if (year%400 == 0 ){
+            divisibleBy400 = true
+        }else {
+            divisibleBy400 = false
+        }
+    }
+    if (divisibleBy4 == true){
+        if (divisibleBy100 == false){
+            return true
+        }else if (divisibleBy100 == true){
+            if (divisibleBy400 == true){
+                return true
+            }
+        }else{
+            return false
+        }
+    }else {
+        return false
+    }
+    if (divisibleBy100 == true && divisibleBy400 == false){
+        return false
+    }else if (divisibleBy100 == true && divisibleBy400 == true){
+        return true
+    }
+}
 // Do not edit below this line
 module.exports = leapYears;
+
+
+// if (year % 4 == 0 && year % 100 != 0){
+//     return true;
+// }else if (year % 4 == 0 && year % 100 == 0){
+//     if(year % 400 == 0){
+//         return true;
+//     }
+// }else{
+//     return false
+// }
