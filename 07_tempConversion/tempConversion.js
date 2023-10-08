6b2205f1f28718b29d460d822950d382527c0866
pharma <92540444+GreatLoveV@users.noreply.github.com>
2023-10-08 18:23:50 +0100

    Completed the seventh ex easily applied the normal formula used toFixed(1) for the rounding, plus the unary plus to convert the result to a rounded number
    till next time

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp1) {
+  return  +((5/9 * (temp1-32)).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp2) {
+   return +((temp2 * (9/5) + 32).toFixed(1))
 };
 
 // Do not edit below this line
